5feb3ddbbd64502ddaa490489f0436290ec4f6d2
Parth Gupta <parthgupta3003@gmail.com>
2023-04-20 14:11:03 +0530

    1done

--- server/app.py
+++ server/app.py
@@ -1,43 +1,59 @@
-import cv2
 import numpy as np
-from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.applications.efficientnet import EfficientNetB0
+import imutils
+import time
+import cv2
 
-# Load the pre-trained model
-model = EfficientNetB0(weights='imagenet')
+CLASSES = ["aeroplane", "background", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor",    "Desk",    "computer",    "monitor",    "keyboard",    "mouse",    "printer",    "scanner",    "faxmachine",    "telephone",    "calculator",    "Notepad",    "Pen",    "Pencil",    "Highlighter",    "Stapler",    "Paperclip",    "Tape",    "Glue",    "Envelopes",    "Folders",    "File cabinet",    "Whiteboard",    "Markers",    "Post-it notes",    "Push pins",    "Ruler",    "Scissors",    "Paper",    "Binder clips",    "Rubber bands",    "Trash can",    "Desk lamp",    "Wall clock",    "Calendar",    "Name plate",    "Coat rack"]
 
-# Initialize the camera
-cap = cv2.VideoCapture(0)
+
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+net = cv2.dnn.readNetFromCaffe("MobileNetSSD_deploy.prototxt.txt","MobileNetSSD_deploy.caffemodel")
+
+cap = cv2.VideoCapture("")
+
+time.sleep(2.0)
 
 while True:
-    # Read the frame from the camera
-    ret, frame = cap.read()
+	# grab the frame from the video capture and resize it to have a maximum width of 400 pixels
+	_, frame = cap.read()
+	frame = imutils.resize(frame, width=400)
+	(h, w) = frame.shape[:2]
 
-    # Resize the frame to the input size of the model
-    resized_frame = cv2.resize(frame, (224, 224))
+	resized_image = cv2.resize(frame, (300, 300))
 
+	blob = cv2.dnn.blobFromImage(resized_image, (1/127.5), (300, 300), 127.5, swapRB=True)
 
-    # Preprocess the input image
-    x = img_to_array(resized_frame)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+	net.setInput(blob)
 
-    # Make a prediction
-    predictions = model.predict(x)
-    decoded_predictions = decode_predictions(predictions)
+	predictions = net.forward()
 
-    # Get the predicted class label and display it on the frame
-    label = decoded_predictions[0][0][1]
-    cv2.putText(frame, label, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+	# loop over the predictions
+	for i in np.arange(0, predictions.shape[2]):
+		confidence = predictions[0, 0, i, 2]
+		
+		if confidence > 0.8:
+			idx = int(predictions[0, 0, i, 1])
 
-    # Display the frame
-    cv2.imshow('Real-time Image Recognition', frame)
+			box = predictions[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
 
-    # Exit if the user presses the 'q' key
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+			# Get the label with the confidence score
+			label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+			xx=label
 
-# Release the camera and close the window
-cap.release()
-cv2.destroyAllWindows()
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				COLORS[idx], 2)
+			y = startY - 15 if startY - 15 > 15 else startY + 15
+
+			cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	if key == ord("q"):
+		break
+
